7237c5270a1d1b6844532588478ae6a6c7df6939
Duncan Cameron <phplist@dcameron.me.uk>
2023-02-13 16:26:55 +0000

    Update cs fixer

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -5,21 +5,26 @@ $finder = PhpCsFixer\Finder::create()
     ->exclude('tests')
 ;
 
-return PhpCsFixer\Config::create()
-    ->setRules([
+$config = new PhpCsFixer\Config();
+
+return $config->setRules([
         '@PSR1' => true,
         '@PSR2' => true,
         '@Symfony' => true,
         'concat_space' => false,
         'phpdoc_no_alias_tag' => false,
         'yoda_style' => false,
-        'array_syntax' => null,
-        'no_superfluous_phpdoc_tags' => null,
+        'array_syntax' => false,
+        'no_superfluous_phpdoc_tags' => false,
         'ordered_imports' => [
             'sort_algorithm' => 'alpha',
-            'imports_order' => ['class', 'function', 'const']
+            'imports_order' => ['class', 'function', 'const'],
         ],
         'blank_line_after_namespace' => true,
+        'single_line_comment_style' => false,
+        'visibility_required' => false,
+        'phpdoc_to_comment' => false,
+        'function_typehint_space' => false,
     ])
     ->setFinder($finder)
 ;
--- plugins/RssFeedPlugin/class_map.php
+++ plugins/RssFeedPlugin/class_map.php
@@ -1,12 +1,14 @@
 <?php
 
-return array(
-    'phpList\plugin\RssFeedPlugin\ControllerFactory' => $base . '/RssFeedPlugin/ControllerFactory.php',
-    'phpList\plugin\RssFeedPlugin\Controller\Delete' => $base . '/RssFeedPlugin/Controller/Delete.php',
-    'phpList\plugin\RssFeedPlugin\Controller\Feeds' => $base . '/RssFeedPlugin/Controller/Feeds.php',
-    'phpList\plugin\RssFeedPlugin\Controller\Get' => $base . '/RssFeedPlugin/Controller/Get.php',
-    'phpList\plugin\RssFeedPlugin\Controller\TwitterFeed' => $base . '/RssFeedPlugin/Controller/Twitterfeed.php',
-    'phpList\plugin\RssFeedPlugin\Controller\View' => $base . '/RssFeedPlugin/Controller/View.php',
-    'phpList\plugin\RssFeedPlugin\DAO' => $base . '/RssFeedPlugin/DAO.php',
-    'phpList\plugin\RssFeedPlugin\TwitterParser' => $base . '/RssFeedPlugin/TwitterParser.php',
-);
+$pluginsDir = dirname(__DIR__);
+
+return [
+    'phpList\plugin\RssFeedPlugin\ControllerFactory' => $pluginsDir . '/RssFeedPlugin/ControllerFactory.php',
+    'phpList\plugin\RssFeedPlugin\Controller\Delete' => $pluginsDir . '/RssFeedPlugin/Controller/Delete.php',
+    'phpList\plugin\RssFeedPlugin\Controller\Feeds' => $pluginsDir . '/RssFeedPlugin/Controller/Feeds.php',
+    'phpList\plugin\RssFeedPlugin\Controller\Get' => $pluginsDir . '/RssFeedPlugin/Controller/Get.php',
+    'phpList\plugin\RssFeedPlugin\Controller\TwitterFeed' => $pluginsDir . '/RssFeedPlugin/Controller/Twitterfeed.php',
+    'phpList\plugin\RssFeedPlugin\Controller\View' => $pluginsDir . '/RssFeedPlugin/Controller/View.php',
+    'phpList\plugin\RssFeedPlugin\DAO' => $pluginsDir . '/RssFeedPlugin/DAO.php',
+    'phpList\plugin\RssFeedPlugin\TwitterParser' => $pluginsDir . '/RssFeedPlugin/TwitterParser.php',
+];
